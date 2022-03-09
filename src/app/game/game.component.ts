4bfd4b8732d49871ade50ebe238ad6cbd8f049b2
Michelle <michelle-heintze@web.de>
2022-03-09 15:16:02 +0100

    add players and btn

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 
 @Component({
@@ -9,10 +11,10 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  currentCard: string = '';
+ currentCard:string = '';
   game: Game = new Game;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
@@ -24,7 +26,34 @@ console.log(this.game);
   }
 
   takeCard(){
+    if(!this.pickCardAnimation){// nur wenn(!Variable false ist) wird es ausgeführt. Wir können also nur noch alle 1500sek ausführen.
+ this.currentCard = this.game.stack.pop();
+    //mit this.game greifen wir auf aud Array zu mit pop() greifen wir auf den 
+    //letzten Wert des Arrays zu gleichzeitig wird er aus dem Array entfernt#
     this.pickCardAnimation = true;
+    console.log(this.currentCard);
+    console.log(this.game);
+
+    setTimeout(()=>{
+      //Sobald die Animation fertig ist wird die Karte dem Array hinzugefügt
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;//Animation wird zurück auf false gesetzt, damit sie erneut ausgeführt werden kann.
+      //Die gezeigte Karte verschwindet und geht wieder ins Deck. 
+    },1000);
+    }
+
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
+
+    
+  
+
+
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = []; 
+    public players: string[] = ['Hilde', 'Trude','Heinrich', 'Hubert']; 
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
